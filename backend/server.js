6eb5865fc8a0d9b50b48a24b40b75a888d30c6f2
John Dale <113763700+dingdingwall@users.noreply.github.com>
2023-12-11 20:27:40 +0800

    wala na finish na
    hahahaha yey

--- backend/server.js
+++ backend/server.js
@@ -76,3 +76,27 @@ app.listen(3001, () => {
 
      console.log("connected to backend");
 })
+
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+  
+    // Check if username and password are provided
+    if (!username || !password) {
+      return res.status(400).json({ message: 'Username and password are required' });
+    }
+  
+    // Check user in the database using parameterized query
+    const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
+    db.query(query, [username, password], (err, results) => {
+      if (err) {
+        return res.status(500).json({ message: 'Internal server error' });
+      }
+  
+      if (results.length === 0) {
+        return res.status(401).json({ message: 'Invalid username or password' });
+      }
+  
+      res.status(200).json({ message: 'Login successful' });
+    });
+  });
