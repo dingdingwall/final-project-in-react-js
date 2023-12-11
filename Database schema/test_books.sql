84042477067390e7d9802f6714689ea0d5bfae01
John Dale <113763700+dingdingwall@users.noreply.github.com>
2023-12-11 20:34:59 +0800

    Add files via upload

--- Database schema/test_books.sql
+++ Database schema/test_books.sql
@@ -31,7 +31,7 @@ CREATE TABLE `books` (
   `desc` varchar(45) NOT NULL,
   `cover` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +40,6 @@ CREATE TABLE `books` (
 
 LOCK TABLES `books` WRITE;
 /*!40000 ALTER TABLE `books` DISABLE KEYS */;
-INSERT INTO `books` VALUES (34,'Jack Frost',0,'Jack Frost is a name and snow','400');
 /*!40000 ALTER TABLE `books` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-12-09 16:06:36
+-- Dump completed on 2023-12-11 20:22:52
